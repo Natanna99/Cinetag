b917df8afc9c0afe2ad463950df6530335da73bb
Natanna99 <erzafairy79@gmail.com>
2025-01-24 21:59:08 -0300

    Chore: import de react bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import AppRouters from "./routers/routes";
 import "./fontawasone";
