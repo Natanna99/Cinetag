56b6a54d690851cdd30fae43dde09c4b0c9d23e8
Natanna99 <erzafairy79@gmail.com>
2025-01-24 22:09:02 -0300

    Feature: componente de footer

--- src/pages/pageBase/index.js
+++ src/pages/pageBase/index.js
@@ -1,3 +1,4 @@
+import { Footer } from "components/footer";
 import { Header } from "components/header";
 import { FilmProvide } from "context/filmContext";
 import { Outlet } from "react-router-dom";
@@ -9,6 +10,7 @@ export function PageBase() {
       <FilmProvide>
         <Outlet />
       </FilmProvide>
+      <Footer />
     </div>
   );
 }
