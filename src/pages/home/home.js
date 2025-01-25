bb856eae62cabe18c88c88076145271e71392e9e
Natanna99 <erzafairy79@gmail.com>
2025-01-24 21:58:06 -0300

    Feature: pagina de inicio

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,10 +1,30 @@
-import { Header } from "components/header";
+import { Banner } from "components/banner";
+import style from "./home.module.css";
+import { Card } from "components/card";
+import { useContext } from "react";
+import { FilmContext } from "context/filmContext";
 
 export function Home() {
+  const { listLike } = useContext(FilmContext);
+
   return (
     <div>
-      <Header />
-      <h1>Home</h1>
+      <Banner />
+      <div className={style.containerHome}>
+        <h3>Videos disponiveis</h3>
+      </div>
+
+      <div className={style.containerHome}>
+        {listLike?.map((item) => (
+          <Card
+            key={item.id}
+            title={item?.title}
+            img={item?.capa}
+            id={item?.id}
+            likeFilm={item?.like}
+          />
+        ))}
+      </div>
     </div>
   );
 }
